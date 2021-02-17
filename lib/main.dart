510b4f46549d0794f640e2eae85e99f7f125866c
SeanAlexWall <seanalexwall@gmail.com>
2021-02-17 17:02:21 -0600

    init

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Lesson2App());
+}
+
+class Lesson2App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Text("Lesson2 App"),
+    );
+  }
+  
+}
